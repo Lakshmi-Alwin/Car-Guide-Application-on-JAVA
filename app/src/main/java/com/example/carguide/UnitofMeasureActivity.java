bbbba30343be4146deea0d4192ccbcc0815d7662
Jason <jasonjerome16@gmail.com>
2019-06-25 16:59:59 +0530

    Fixed Bug #4 - UnitOfMeasure radio buttons were not saving settings

--- app/src/main/java/com/example/carguide/UnitofMeasureActivity.java
+++ app/src/main/java/com/example/carguide/UnitofMeasureActivity.java
@@ -9,12 +9,34 @@ import android.widget.RadioButton;
 
 public class UnitofMeasureActivity extends AppCompatActivity {
 
+    private RadioButton km_button;
+    private RadioButton mi_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unitof_measure);
         ImageView left = findViewById(R.id.vector_left);
         left.setOnClickListener(v -> UnitofMeasureActivity.super.onBackPressed());
+
+        km_button = findViewById(R.id.radio_km);
+        mi_button = findViewById(R.id.radio_miles);
+
+        setRadioButtons();
+    }
+
+    public void setRadioButtons() {
+        String chosenOption = PreferencesManager.getUnitOfMeasurement();
+        if (chosenOption.equals("Kilometer")) {
+            km_button.setChecked(true);
+        }
+        else if(chosenOption.equals("Miles")) {
+            mi_button.setChecked(true);
+        }
+        else {
+            km_button.setChecked(true);
+            PreferencesManager.saveUnitOfMeasurement("Kilometer");
+        }
     }
 
     public void onRadioButtonClicked(View view) {
