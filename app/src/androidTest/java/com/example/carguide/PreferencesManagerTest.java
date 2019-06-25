5d811bbc87df5dea6e07b770b4bd75af65c1d153
Jason <jasonjerome16@gmail.com>
2019-06-25 15:55:50 +0530

    Optimized JUnit Tests
    Initialized shared preferences before tests are run so that it doesn't have to be re-initialized for every test.

--- app/src/androidTest/java/com/example/carguide/PreferencesManagerTest.java
+++ app/src/androidTest/java/com/example/carguide/PreferencesManagerTest.java
@@ -1,6 +1,8 @@
 package com.example.carguide;
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Before;
 import org.junit.Test;
 import static android.content.Context.MODE_PRIVATE;
 import static org.junit.Assert.assertEquals;
@@ -16,59 +18,49 @@ public class PreferencesManagerTest {
     private static String phone = "12345";
     private static String address = "MyAddress";
 
+    @Before
+    public void prepareSharedPreferences() {
+        PreferencesManager.sharedAccountID = sharedPreferencesAccountID;
+        PreferencesManager.sharedPreferences = context.getSharedPreferences(PreferencesManager.sharedAccountID, MODE_PRIVATE);
+    }
 
     @Test
     public void saveClientNameInSharedPreferences() {
-        PreferencesManager.sharedAccountID = sharedPreferencesAccountID;
-        PreferencesManager.sharedPreferences = context.getSharedPreferences(PreferencesManager.sharedAccountID, MODE_PRIVATE);
         PreferencesManager.saveName(name);
         String result = PreferencesManager.getName();
 
-        // ...then the result should be the expected one.
         assertEquals(name, result);
     }
 
     @Test
     public void saveClientPhoneNumberInSharedPreferences() {
-        PreferencesManager.sharedAccountID = sharedPreferencesAccountID;
-        PreferencesManager.sharedPreferences = context.getSharedPreferences(PreferencesManager.sharedAccountID, MODE_PRIVATE);
         PreferencesManager.savePhoneNumber(phone);
         String result = PreferencesManager.getPhoneNumber();
 
-        // ...then the result should be the expected one.
         assertEquals(phone, result);
     }
 
     @Test
     public void saveClientAddressInSharedPreferences() {
-        PreferencesManager.sharedAccountID = sharedPreferencesAccountID;
-        PreferencesManager.sharedPreferences = context.getSharedPreferences(PreferencesManager.sharedAccountID, MODE_PRIVATE);
         PreferencesManager.saveAddress(address);
         String result = PreferencesManager.getAddress();
 
-        // ...then the result should be the expected one.
         assertEquals(address, result);
     }
 
     @Test
     public void saveVehicleVINInSharedPreferences() {
-        PreferencesManager.sharedAccountID = sharedPreferencesAccountID;
-        PreferencesManager.sharedPreferences = context.getSharedPreferences(PreferencesManager.sharedAccountID, MODE_PRIVATE);
         PreferencesManager.saveVIN(vin);
         String result = PreferencesManager.getVIN();
 
-        // ...then the result should be the expected one.
         assertEquals(vin, result);
     }
 
     @Test
     public void saveVehicleNickNameInSharedPreferences() {
-        PreferencesManager.sharedAccountID = sharedPreferencesAccountID;
-        PreferencesManager.sharedPreferences = context.getSharedPreferences(PreferencesManager.sharedAccountID, MODE_PRIVATE);
         PreferencesManager.saveVehicleNickName(vehicle_nickname);
         String result = PreferencesManager.getVehicleNickname();
 
-        // ...then the result should be the expected one.
         assertEquals(vehicle_nickname, result);
     }
 }
