70c23dc213c76911ecc6d5c890dc84578258830d
Shirswar <vhmshirswar8991@gmail.com>
2019-07-03 12:14:43 +0530

    Addvehicle api refactored

--- app/src/main/java/com/example/carguide/ApiSingleton.java
+++ app/src/main/java/com/example/carguide/ApiSingleton.java
@@ -5,7 +5,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
-public class ApiSingleton {
+class ApiSingleton {
     private static ApiSingleton instance;
     private RequestQueue requestQueue;
     private static Context ctx;
@@ -15,14 +15,14 @@ public class ApiSingleton {
         requestQueue = getRequestQueue();
     }
 
-    public static synchronized ApiSingleton getInstance(Context context) {
+    static synchronized ApiSingleton getInstance(Context context) {
         if (instance == null) {
             instance = new ApiSingleton(context);
         }
         return instance;
     }
 
-    public RequestQueue getRequestQueue() {
+    private RequestQueue getRequestQueue() {
         if (requestQueue == null) {
             // getApplicationContext() is key, it keeps you from leaking the
             // Activity or BroadcastReceiver if someone passes one in.
