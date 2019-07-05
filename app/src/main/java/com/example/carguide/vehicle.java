435200c8c9201c56002f74c295b094801a2a67cb
Shirswar <vhmshirswar8991@gmail.com>
2019-07-05 11:40:11 +0530

    Gestures and animation on Vehicle fragment

--- app/src/main/java/com/example/carguide/vehicle.java
+++ app/src/main/java/com/example/carguide/vehicle.java
@@ -1,11 +1,15 @@
 package com.example.carguide;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -13,21 +17,33 @@ import android.widget.TextView;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
+import java.util.Objects;
+
 
 public class vehicle extends Fragment {
 
+    View v;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v= inflater.inflate(R.layout.fragment_vehicle,container,false);
+        v= inflater.inflate(R.layout.fragment_vehicle,container,false);
 
         TextView profileName = v.findViewById(R.id.vehicle_page_name);
         GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(getActivity());
         ImageView vehicle = v.findViewById(R.id.car_vehiclepage);
-        vehicle.setOnClickListener(v1 -> {
-            Intent intent = new Intent(v1.getContext(), AddVehicle.class);
-            startActivity(intent);
+//        vehicle.setOnClickListener(v1 -> {
+//            Intent intent = new Intent(v1.getContext(), AddVehicle.class);
+//            startActivity(intent);
+//        });
+
+        GestureDetector gdt = new GestureDetector(new GestureListner());
+        vehicle.setOnTouchListener((view, motionEvent) -> {
+            gdt.onTouchEvent(motionEvent);
+            return true;
         });
+
         if(!PreferencesManager.sharedPreferences.contains(PreferencesManager.CLIENT_NAME))
             profileName.setText(googleSignInAccount.getDisplayName());
         else
@@ -35,4 +51,18 @@ public class vehicle extends Fragment {
         return v;
     }
 
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+    private class GestureListner extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if(e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                Intent intent = new Intent(v.getContext(), AddVehicle.class);
+                startActivity(intent);
+                Objects.requireNonNull(getActivity()).overridePendingTransition(R.anim.slide_up, R.anim.slide_down);
+            }
+            return false;
+        }
+    }
 }
